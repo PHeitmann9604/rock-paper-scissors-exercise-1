f7d4021f3f1041e0010d4de54ca9f2c0f2eadeae
PHeitmann9604 <pjh9604@stern.nyu.edu>
2021-07-08 20:37:14 -0400

    Capture user input

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
 print("Rock, Paper, Scissors, Shoot!")
+
+# ask for a user input
+# source *put google doc here*
+#x = input('______')
+
+x = input("Please choose one of 'rock', 'paper', 'scissors': ")
+print(x)
+
+# Validate a user input
+
+#Generate a computer choice
+
+#Determine the winner
+
